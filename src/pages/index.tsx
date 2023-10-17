1ae59b8e3d5bebc80b53b02f6ec7ff6d5418ca9a
liem_nguyen_thanh <liemdonduong@gmail.com>
2023-10-17 08:36:54 +0700

    chore: add husky + created doc, app

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="bg-slate-100 rounded-xl dark:bg-slate-800">
+      <p className="text-lg text-center p-10 m-10">Home page</p>
+    </div>
+  );
+}
