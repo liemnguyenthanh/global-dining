fc4f985ae7fc1b298dd47431ad7b8b4f25c53ff4
liem_nguyen_thanh <liemdonduong@gmail.com>
2023-10-17 09:27:54 +0700

    chore: add scss

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import '@/styles/globals.css';
+import '@/assets/scss/style.scss';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
